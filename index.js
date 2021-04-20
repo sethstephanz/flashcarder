ea5187bfe73c9381658284cfa4e8d9c7f3c1c91d
Seth Stephanz <sethstephanz@gmail.com>
2021-04-20 17:38:33 -0400

    Add some routes and some main pages for the app.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home.ejs", { title: "Flashcarder" });
+});
+
+app.get("/stacks", (req, res) => {
+  res.render("stacks.ejs", { title: "Stacks" });
+});
+
+app.get("/stats", (req, res) => {
+  res.render("stats.ejs", { title: "Stats" });
 });
 
 app.listen(3000, () => {
